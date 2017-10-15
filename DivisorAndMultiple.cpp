1c0029b783300ee7924eb9a37728c67d06644966
GaoLing <1404919041@qq.com>
2017-10-15 11:25:03 +0800

    i finished Gcd and GcdXunHuan and other things but i should add more!

--- DivisorAndMultiple.cpp
+++ DivisorAndMultiple.cpp
@@ -33,23 +33,37 @@ int Gcd(int A,int B)
   
 }
 
+int GcdXunHuan(int L,int R)
+{
+  int temp;
+  while((temp=L%R)!=0)
+  {
+    L=R;
+    R=temp;
+  }
+  return R;
+}
 
 int MoreSubtractiveMethod(int A, int B,int N=0)
 {
   if (A<B) {
-    swap(&A,&B);
+    Swap(&A,&B);
   }
 
-  if (A-B=B) {
+  if (A-B==B) {
     return N*2+B;
   }
 
 
-  if (IsEven(A)&&isEven(B)) {
+  if (IsEven(A)&&IsEven(B)) {
     return MoreSubtractiveMethod(A,B,N+1);
   }
   else if(A-B>B){
-    return MoreSubtractiveMethod(A,B,N)
+    return MoreSubtractiveMethod(A-B,B,N);
+  }
+  else {
+    return MoreSubtractiveMethod(B,A-B,N);
+    
   }
 }
 
@@ -58,9 +72,23 @@ int main()
 {
   //get input x0 y0
   int x0,y0;
-  cin>>x0>>y0;
+  int sum=0;
+  int p;
+  int minin;
+  int maxin;
 
-  cout<<Gcd(x0,y0)<<endl;
+  cin>>x0>>y0;
+  int limit=x0*y0;
+  minin=x0>y0?y0:x0;
+  maxin=x0>y0?x0:y0;
+
+  for (p=minin;p<=maxin;p++)
+  {
+    if (limit%p==0&&GcdXunHuan(p,limit/p)==x0) sum++;
+    
+  }
+
+  cout<<sum<<endl;
 
 
   
