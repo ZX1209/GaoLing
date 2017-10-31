776252dd72b02917df8416f9386f665e840edd8c
ZX1209 <1404919041@QQ.COM>
2017-10-31 18:25:39 +0800

    start to STL add stack exercise

---
+++ STLstack.cpp
@@ -0,0 +1,72 @@
+#include<stack>
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+
+void MessageLoop()
+{
+	char opt='\0';
+	char str[100]={'\0'};
+	scanf("%c %s",&opt,str);
+	switch(opt)
+	{
+		case 'p':
+			printf("%s\n",str);
+			break;
+		case 'b':
+			printf("this is what you want??\n");
+			break;
+		default:
+			printf("not correct!\n");
+			
+			break;
+	}
+	
+	
+} 
+
+int main()
+{
+	char opt='\0';
+	char str[100]={'\0'};
+	int i=0;
+	
+	char c;
+	while(1)
+	{
+		
+		if(scanf("command:%c;",&opt)==1)
+		{
+			switch(opt)
+			{
+				case 'p':
+					getchar();
+					
+					while((c=getchar())!='\n'&&c!=EOF){
+						str[i++]=c;
+					 }
+					 str[i]='\0';
+					 i=0;
+					printf("%s\n",str);
+					break;
+				case 'b':
+					printf("this is what you want??\n");
+					break;
+				default:
+					printf("not correct!\n");
+					
+					break;
+			}
+			
+		}
+		else{
+			while((c=getchar())!='\n'&&c!=EOF);
+		}
+		
+    }
+	
+	
+	return 0;
+} 
