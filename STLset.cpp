c31e9febabf35a11f7c3781f7670a1b2f171753c
ZX1209 <1404919041@QQ.COM>
2017-10-31 20:15:30 +0800

    i am going to find the book written by liu nv jia

--- STLset.cpp
+++ STLset.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 #include<set>
+#include<string>
 
-using namespce std;
+using namespace std;
 
 int main()
 {
+	set<string> str;
+	str.insert("apple");
+	str.insert("orange");
+	str.insert("banana");
+	str.insert("grapes");
+	set<string>::iterator pos;
 	
+	for(pos=str.begin();pos!=str.end();pos++)
+	{
+		cout<<*pos<<" ";
+	}
+	cout<<endl;
+	str.clear();
+	return 0;
 }
