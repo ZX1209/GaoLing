d82f5f59b4fb458798523f58c93b04cc446b73c0
ZX1209 <1404919041@QQ.COM>
2017-11-01 21:00:40 +0800

    what have i done?ou .... 回溯算法!!!!

--- STLqueue.cpp
+++ STLqueue.cpp
@@ -5,6 +5,20 @@ using namespace std;
 
 int main()
 {
+	queue<int> q;
+	q.push(1);q.push(2);q.push(3);q.push(4);
+	cout<<q.size()<<endl;
+	cout<<q.empty()<<endl;
+	cout<<q.front()<<endl;
+	cout<<q.back()<<endl;
+	
+	while(q.empty()!=true)
+	{
+		cout<<q.front()<<" ";
+		q.pop();
+	}
+	
+	
 	
 	
 	return 0;
